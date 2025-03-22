440b2d14be4ae9c89ee506377aadff17f167ee41
Harsh Rohila <rohilaharsh@gmail.com>
2025-03-22 21:21:08 +0530

    configured vite to use react

---
+++ src/views/web/react-components/Root.tsx
@@ -0,0 +1,7 @@
+import { MAX_CARDS_TO_PLAY_IN_ONE_TURN } from "@/constants"
+
+function Root() {
+  return <h1>harsh {MAX_CARDS_TO_PLAY_IN_ONE_TURN}</h1>
+}
+
+export { Root }
---
+++ src/views/web/react-components/index.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client"
+import { Root } from "./Root"
+
+// Render your React component instead
+const root = createRoot(document.getElementById("root"))
+root.render(<Root />)
